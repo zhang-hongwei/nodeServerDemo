43057864ebd7679163c775495a3b943fbd44b35e
余笙 <zhanghw2@flamelephant.com>
2023-04-13 16:06:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/getList", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(6000, () => {
+  console.log("Server started on port 3000");
+});
