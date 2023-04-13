1f54beaa76b2b3d01563cde0ac4866bf4edbdd2e
余笙 <zhanghw2@flamelephant.com>
2023-04-13 16:09:47 +0800

    修改端口

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/getList", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(6000, () => {
-  console.log("Server started on port 3000");
+app.listen(8800, () => {
+  console.log("Server started on port 8800");
 });
